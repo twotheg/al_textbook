516ecf7b7fdc7375678f62ec12aea5e3e929be92
twotheg <twotheg@gmail.com>
2026-08-29 00:52:06 +0900

    Update icon paths in manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,19 +14,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0ea5e9",
     icons: [
       {
-        src: "/icons/icon-192x192.png",
+        src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/icon-512x512.png",
+        src: "/icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icons/maskable-icon-512x512.png",
+        src: "/maskable-icon-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
